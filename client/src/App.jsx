9c391cd36fafde510fef6c8a09802767bb5f31f5
nikhilreddy447 <aletinikhilreddy759@gmail.com>
2023-12-25 18:23:46 +0530

    Made steup for the project

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className='px-16 py-20'>
+      <button className='text-red-200 bg-gray-400 flex items-center justify-center w-max-[200px]'> Hello world!</button>
+    </div>
+  )
+}
