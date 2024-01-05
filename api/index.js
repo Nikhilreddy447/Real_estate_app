1dbbdb32631d2b7f78aa8af5fe4619d4f1c29df7
nikhilreddy447 <aletinikhilreddy759@gmail.com>
2024-01-05 22:30:35 +0530

    Connected to MongoDB Database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose
+    .connect("mongodb+srv://nikhilreddyaleti2002:nikhil@mern-estate.m9vml0d.mongodb.net/mern-estate?retryWrites=true&w=majority")
+    .then(()=>{
+        console.log("Connected to MongoDb")
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
 
 const app = express();
 
