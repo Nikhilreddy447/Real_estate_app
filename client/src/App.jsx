1b7a18b7a368f14ae221922dfe014b7d423c0f5c
nikhilreddy447 <aletinikhilreddy759@gmail.com>
2024-01-05 17:23:07 +0530

    created pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home.jsx';
+import Signin from './pages/Signin.jsx';
+import SignUp from './pages/SignUp.jsx';
+import About from './pages/About.jsx';
+import Profile from './pages/Profile.jsx';
 
 export default function App() {
+  
   return (
-    <div className='px-16 py-20'>
-      <button className='text-red-200 bg-gray-400 flex items-center justify-center w-max-[200px]'> Hello world!</button>
-    </div>
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/sign-in' element={<Signin />} />
+      <Route path='/sign-up' element={<SignUp />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/profile' element={<Profile />} />
+    </Routes>
+  </BrowserRouter>
   )
 }
